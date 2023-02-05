c1515d796035724f910114005c0386e629f47a79
ShawnLee21 <85219616+ShawnLee21@users.noreply.github.com>
2023-02-05 17:16:27 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,116 @@
+let screenPointEl1 = document.getElementById("pointScreen1")
+let screenPointEl2 = document.getElementById("pointScreen2")
+let screenPointEl3 = document.getElementById("pointScreen3")
+let screenPointEl4 = document.getElementById("pointScreen4")
+let screenPointEl5 = document.getElementById("pointScreen5")
+let screenPointEl6 = document.getElementById("pointScreen6")
+let countMachine1 = 0
+let countMachine2 = 0
+let countMachine3 = 0
+let countMachine4 = 0
+let countMachine5 = 0
+let countMachine6 = 0
+
+
+function plus1Screen1() {
+    countMachine1 += 10
+    screenPointEl1.textContent = countMachine1
+}
+function plus2Screen1() {
+    countMachine1 += 20
+    screenPointEl1.textContent = countMachine1
+}
+function plus4Screen1() {
+    countMachine1 += 40
+    screenPointEl1.textContent = countMachine1
+}
+function clearScreen1() {
+    countMachine1 = 0
+    screenPointEl1.textContent = countMachine1
+}
+
+
+function plus1Screen2() {
+    countMachine2 += 10
+    screenPointEl2.textContent = countMachine2
+}
+function plus2Screen2() {
+    countMachine2 += 20
+    screenPointEl2.textContent = countMachine2
+}
+function plus4Screen2() {
+    countMachine2 += 40
+    screenPointEl2.textContent = countMachine2
+}
+function clearScreen2() {
+    countMachine2 = 0
+    screenPointEl2.textContent = countMachine2
+}
+
+function plus1Screen3() {
+    countMachine3 += 10
+    screenPointEl3.textContent = countMachine3
+}
+function plus2Screen3() {
+    countMachine3 += 20
+    screenPointEl3.textContent = countMachine3
+}
+function plus4Screen3() {
+    countMachine3 += 40
+    screenPointEl3.textContent = countMachine3
+}
+function clearScreen3() {
+    countMachine3 = 0
+    screenPointEl3.textContent = countMachine3
+}
+
+function plus1Screen4() {
+    countMachine4 += 10
+    screenPointEl4.textContent = countMachine4
+}
+function plus2Screen4() {
+    countMachine4 += 20
+    screenPointEl4.textContent = countMachine4
+}
+function plus4Screen4() {
+    countMachine4 += 40
+    screenPointEl4.textContent = countMachine4
+}
+function clearScreen4() {
+    countMachine4 = 0
+    screenPointEl4.textContent = countMachine4
+}
+
+function plus1Screen5() {
+    countMachine5 += 10
+    screenPointEl5.textContent = countMachine5
+}
+function plus2Screen5() {
+    countMachine5 += 20
+    screenPointEl5.textContent = countMachine5
+}
+function plus4Screen5() {
+    countMachine5 += 40
+    screenPointEl5.textContent = countMachine5
+}
+function clearScreen5() {
+    countMachine5 = 0
+    screenPointEl5.textContent = countMachine5
+}
+
+function plus1Screen6() {
+    countMachine6 += 10
+    screenPointEl6.textContent = countMachine6
+}
+function plus2Screen6() {
+    countMachine6 += 20
+    screenPointEl6.textContent = countMachine6
+}
+function plus4Screen6() {
+    countMachine6 += 40
+    screenPointEl6.textContent = countMachine6
+}
+function clearScreen6() {
+    countMachine6 = 0
+    screenPointEl6.textContent = countMachine6
+}
